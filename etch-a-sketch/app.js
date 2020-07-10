5218a33dd23dd1fec5a41b1d2971e0b0bed1d32a
Horace Crump <hcrump@gmail.com>
2020-07-10 11:48:40 -0700

    updated etch..

--- etch-a-sketch/app.js
+++ etch-a-sketch/app.js
@@ -1,7 +1,9 @@
 // Initial variables
-let defaultGridSize = 16;
+const defaultGridSize = 16;
+const maxGridSize = 100;
 let currentGridSize = defaultGridSize;
 let drawColor = 'default';
+let isDrawing = false;
 const container = document.querySelector('#container');
 const gridSection = document.querySelector('#gridSection');
 const btnReset = document.querySelector('#btnReset');
@@ -11,6 +13,65 @@ const btnEraser = document.querySelector('#btnEraser');
 const btn32 = document.querySelector('#btn32');
 const btn48 = document.querySelector('#btn48');
 const btn16 = document.querySelector('#btn16');
+
+// Initialize page at start to basic grid
+createGrid(defaultGridSize);
+
+function createGrid(gridSize){
+    if(gridSize < defaultGridSize || gridSize > maxGridSize){ 
+        gridSize = defaultGridSize;
+        alert(`Sorry, dimensions must range from ${defaultGridSize} to ${maxGridSize}!!`)
+    }
+    
+    currentGridSize = gridSize;
+    let numberOfDivs = gridSize * gridSize;
+    let canvasSize = (600 / gridSize) + "px";
+    
+    gridSection.innerHTML = "";
+    gridSection.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    gridSection.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
+    for (let i = 0; i < numberOfDivs; i++){
+        let box = document.createElement('div');
+        box.className = 'boxes';
+        box.style.width = canvasSize;
+        box.style.height = canvasSize;
+        box.addEventListener('mouseover',changeColor);
+        gridSection.appendChild(box);
+    }
+}
+
+function changeColor(e) {
+    if(isDrawing === false){
+        return;
+    }
+    e.preventDefault;
+    if (drawColor == 'shade'){
+        let bgc = window.getComputedStyle(e.target).backgroundColor;
+        let col = bgc.replace(/[^\d,]/g,'').split(',');
+
+        for( let i in col){
+            col[i] -= Math.round(255 * .1);
+            if (col[i] < 0){
+                col[i] = 0;
+            } 
+        }
+        e.target.style.backgroundColor = `rgb(${col[0]},${col[1]},${col[2]})`;
+    }
+    else if(drawColor == 'black'){     
+        e.target.style.backgroundColor = 'black';
+    }
+    else if(drawColor == 'random'){
+        let bgc = window.getComputedStyle(e.target).backgroundColor;
+        let r = Math.floor((Math.random() * 255) + 1);
+        let g = Math.floor((Math.random() * 255) + 1);
+        let b = Math.floor((Math.random() * 255) + 1);
+        e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+    }
+    else {
+        e.target.style.backgroundColor = 'white';
+    }
+}
 
 // button addEventListeners
 btnReset.addEventListener('click', () => {
@@ -53,58 +114,13 @@ form1.addEventListener('submit', (e) => {
     createGrid(formValue);
 });
 
-// Initialize page at start to basic grid
-createGrid(defaultGridSize);
+// e.preventDefault stops UI action as mouseup's were being missed
+gridSection.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    isDrawing = true;
+});
 
-function createGrid(gridSize){
-    if(gridSize < defaultGridSize || gridSize > 100){ 
-        gridSize = defaultGridSize;
-        alert("Sorry, dimensions must range from 16 to 100!")
-    }
-    
-    currentGridSize = gridSize;
-    let numberOfDivs = gridSize * gridSize;
-    let canvasSize = (600 / gridSize) + "px";
-    
-    gridSection.innerHTML = "";
-    gridSection.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-    gridSection.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-
-    for (let i = 0; i < numberOfDivs; i++){
-        let box = document.createElement('div');
-        box.className = 'boxes';
-        box.style.width = canvasSize;
-        box.style.height = canvasSize;
-        box.addEventListener('mouseover',changeColor);
-        gridSection.appendChild(box);
-    }
-}
-
-function changeColor(e) {
-
-    if (drawColor == 'shade'){
-        let bgc = window.getComputedStyle(e.target).backgroundColor;
-        let col = bgc.replace(/[^\d,]/g,'').split(',');
-
-        for( let i in col){
-            col[i] -= Math.round(255 * .1);
-            if (col[i] < 0){
-                col[i] = 0;
-            } 
-        }
-        e.target.style.backgroundColor = `rgb(${col[0]},${col[1]},${col[2]})`;
-    }
-    else if(drawColor == 'black'){     
-        e.target.style.backgroundColor = 'black';
-    }
-    else if(drawColor == 'random'){
-        let bgc = window.getComputedStyle(e.target).backgroundColor;
-        let r = Math.floor((Math.random() * 255) + 1);
-        let g = Math.floor((Math.random() * 255) + 1);
-        let b = Math.floor((Math.random() * 255) + 1);
-        e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
-    }
-    else {
-        e.target.style.backgroundColor = 'white';
-    }
-}
+window.addEventListener('mouseup', (e) => {
+    e.preventDefault();
+    isDrawing = false;
+});
